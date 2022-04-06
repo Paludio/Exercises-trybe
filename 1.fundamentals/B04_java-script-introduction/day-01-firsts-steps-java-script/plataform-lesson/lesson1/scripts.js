1e1d8d4a8c6d36a5899ba67d073b1bbc62845606
Pablo S. Peixoto <pablonegociosnet@gmail.com>
2022-04-06 16:55:03 -0300

    first lessons of the course

---
+++ 1.fundamentals/B04_java-script-introduction/day-01-firsts-steps-java-script/plataform-lesson/lesson1/scripts.js
@@ -0,0 +1,27 @@
+// const myName = "Pablo Souza Peixoto";
+// const birthCity = "Goiânia";
+// let bithYear = 1997;
+// birthYear = 2030;
+
+
+// console.log(birthYear);
+
+// let movie = "Avengers";
+// let score = 10.76;
+// let isValid = true;
+// let name;
+// let color = null;
+
+let salary = 3500;
+
+// console.log(salary + salary);
+// console.log(salary - salary);
+// console.log(salary * salary);
+// console.log(salary / salary);
+// console.log(3 ** 3);
+
+// salary++;
+
+salary--;
+
+console.log(salary);
---
+++ 1.fundamentals/B04_java-script-introduction/day-01-firsts-steps-java-script/plataform-lesson/lesson2/scripts.js
@@ -0,0 +1,58 @@
+// let trybe = 17;
+
+
+// if (trybe >= 14 && trybe < 14.40) {
+//     console.log("Esquenta");
+// }
+// else if (trybe >= 16.30 && trybe <17.5) {
+//     console.log("aula ao vivo");
+// }
+// else  {
+//     console.log("fechamento");
+// }
+
+// const nota = 59;
+
+// if (nota >= 80) {
+//     console.log("Aprovado(a)");
+// } else if (nota < 80 && nota > 60){
+//     console.log("você está na nossa lista de espera");
+// } else {
+//     console.log("Você foi reprovado(a)");
+// }
+
+// const currentHour = 10;
+// let message ="";
+
+// if (currentHour >= 22) {
+//     message = "Não deveríamos comer nada, é hora de dormir";
+// } else if (currentHour >= 18 && currentHour < 22) {
+//     message = "Rango da noite, vamos jantar :D";
+// } else if (currentHour >= 14 && currentHour < 18) {
+//     message = "Vamos fazer um bolo pro café da tarde?";
+// } else if (currentHour >= 11 && currentHour <=14) {
+//     message = "Hora do almoço!!!";
+// } else if (currentHour >= 4 && currentHour <=11) { 
+//     message = "Hmmm, cheiro de café recém passado";
+// }
+
+// console.log(message);
+
+let trafficLight = "verde";
+
+switch (trafficLight) {
+    case "vermelho":
+        console.log("pare");
+        break;
+
+    case "amarelo":
+    console.log("atenção");
+    break;
+
+    case "verde":
+        console.log("siga");
+        break;
+
+    default:
+        console.log ("valor não identificado");
+}
