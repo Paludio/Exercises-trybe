e3b8d50b6020abf00e9721b30096f7cee71a7c67
Pablo S. Peixoto <pablonegociosnet@gmail.com>
2022-04-06 18:17:53 -0300

    finalizados os exercicios

--- 1.fundamentals/B04_java-script-introduction/day-01-firsts-steps-java-script/plataform-lesson/lesson2/scripts.js
+++ 1.fundamentals/B04_java-script-introduction/day-01-firsts-steps-java-script/plataform-lesson/lesson2/scripts.js
@@ -38,21 +38,22 @@
 
 // console.log(message);
 
-let trafficLight = "verde";
+// let trafficLight = "verde";
 
-switch (trafficLight) {
-    case "vermelho":
-        console.log("pare");
-        break;
+// switch (trafficLight) {
+//     case "vermelho":
+//         console.log("pare");
+//         break;
 
-    case "amarelo":
-    console.log("atenção");
-    break;
+//     case "amarelo":
+//     console.log("atenção");
+//     break;
 
-    case "verde":
-        console.log("siga");
-        break;
+//     case "verde":
+//         console.log("siga");
+//         break;
 
-    default:
-        console.log ("valor não identificado");
-}
+//     default:
+//         console.log ("valor não identificado");
+// }
+
