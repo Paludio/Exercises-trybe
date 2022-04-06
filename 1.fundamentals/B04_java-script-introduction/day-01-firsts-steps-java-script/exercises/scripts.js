752324087c0b3da3b046ff080a7bb2c28b4a164b
Pablo S. Peixoto <pablonegociosnet@gmail.com>
2022-04-06 19:28:09 -0300

    Feito até o exercicio 2 do dia

---
+++ 1.fundamentals/B04_java-script-introduction/day-01-firsts-steps-java-script/exercises/scripts.js
@@ -0,0 +1,18 @@
+let n1 = 5;
+let n2 = 6;
+const n3 = 456;
+const n4 = 286;
+let result ="";
+
+// console.log(n1 + n2);
+// console.log(n1 - n2);
+// console.log(n1 * n2);
+// console.log(n1 / n2);
+// console.log(n1 % n2);
+
+if (n3 > n4) {
+    result = "n3 maior";
+} else {
+    result = "n4 maior";
+}
+console.log(result);
