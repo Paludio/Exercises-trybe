efd641f60f45baa58325eaf7d5e6a70ee161f510
Pablo S. Peixoto <pablonegociosnet@gmail.com>
2022-04-07 22:32:58 -0300

    fazendo o sorteio. numeros sorteados ok

--- 1.fundamentals/B04_java-script-introduction/day-02-array-and-loop=for/exercise-lessons/exercises.js
+++ 1.fundamentals/B04_java-script-introduction/day-02-array-and-loop=for/exercise-lessons/exercises.js
@@ -26,16 +26,16 @@
 //     console.log(index);
 // }
 
-let jogo = [1, 3, 23, 54, 59, 23];
-let sorteio = [];
+let meuJogo = [8, 1, 54, 20, 17, 36];
+let numerosSorteados = [];
 
-for (index = 0; index < 6; index += 1) {
-    numeroAleatorio = Math.floor(Math.random() * 60) + 1;
-    sorteio.push(numeroAleatorio);
+meuJogo.sort();
+
+for (let index = 0; index < 6; index += 1) {
+    let sorteio = Math.floor(Math.random() *60) +1;
+    numerosSorteados.push(sorteio);
+    numerosSorteados.sort();
 }
 
 
-
-
-console.log(sorteio);
-
+console.log(numerosSorteados);
