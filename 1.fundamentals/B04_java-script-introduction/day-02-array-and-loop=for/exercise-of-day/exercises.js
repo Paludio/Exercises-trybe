c61e6abb7e08ec0248f226c74d2d0559afb9e6bd
Pablo S. Peixoto <pablonegociosnet@gmail.com>
2022-04-08 15:48:08 -0300

    Exercise 5 ready

--- 1.fundamentals/B04_java-script-introduction/day-02-array-and-loop=for/exercise-of-day/exercises.js
+++ 1.fundamentals/B04_java-script-introduction/day-02-array-and-loop=for/exercise-of-day/exercises.js
@@ -22,14 +22,25 @@
 
 // console.log(soma / numbers.length);
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
 
-for (let index = 0; index < numbers.length; index += 1) {
-    soma += numbers[index];
+// for (let index = 0; index < numbers.length; index += 1) {
+//     soma += numbers[index];
+// }
+// if (soma / numbers.length > 20) {
+//     console.log("valor maior que 20")
+// } else {
+//     console.log("valor menor ou igual a 20");
+// }
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado = 0;
+
+for (let index = 0; index < numbers.length; index += 1)  {
+    if (resultado < numbers[index]) {
+        resultado = numbers[index];
+    }
 }
-if (soma / numbers.length > 20) {
-    console.log("valor maior que 20")
-} else {
-    console.log("valor menor ou igual a 20");
-}
+
+console.log(resultado);
