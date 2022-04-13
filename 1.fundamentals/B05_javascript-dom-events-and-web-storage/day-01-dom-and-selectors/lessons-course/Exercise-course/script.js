edd974e28f72611de8a5b9e6b9b5c496815214b5
Pablo S. Peixoto <pablonegociosnet@gmail.com>
2022-04-13 17:17:10 -0300

    exercise ready

---
+++ 1.fundamentals/B05_javascript-dom-events-and-web-storage/day-01-dom-and-selectors/lessons-course/Exercise-course/script.js
@@ -0,0 +1,19 @@
+document.getElementById('header-container').style.backgroundColor = 'green'
+
+document.getElementById('container').style.backgroundColor = 'grey';
+
+document.getElementsByTagName('h3')[0].style.backgroundColor = 'purple';
+
+document.getElementsByTagName('h3')[1].style.backgroundColor = 'purple';
+
+document.getElementsByTagName('h3')[2].style.backgroundColor = 'black';
+
+document.getElementsByTagName('h3')[3].style.backgroundColor = 'black';
+
+document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = 'pink';
+
+document.querySelector('.emergency-tasks').style.backgroundColor = 'pink';
+
+document.querySelector('.no-emergency-tasks').style.backgroundColor = 'yellow';
+
+document.getElementsByTagName('footer')[0].style.backgroundColor = 'green';
