04f6f65202508bba5a87685c589c374f9acfc5b8
Pablo S. Peixoto <pablonegociosnet@gmail.com>
2022-04-14 18:51:26 -0300

    exercise 2 ready

--- 1.fundamentals/B05_javascript-dom-events-and-web-storage/day-02-working-with-elements/exercises-of-day/script.js
+++ 1.fundamentals/B05_javascript-dom-events-and-web-storage/day-02-working-with-elements/exercises-of-day/script.js
@@ -1,9 +1,22 @@
-let h1 = document.querySelector('body');
+let h1 = document['body'];
 
-function setH1 (tag, conteudo) {
-    let titulo = document.createElement(tag);
-    titulo.innerText = conteudo;
-    h1.appendChild(titulo);
+function setH1 (tag, content) {
+    let title = document.createElement(tag);
+    title.innerText = content;
+    h1.appendChild(title);
 }
 
 setH1('h1', 'Exercício 5.2 - JavaScript DOM');
+
+//-------------------- exercise 1 -----------------------------
+
+let main1 = document['body'];
+
+function setMain (tag, features) {
+    let contentMain = document.createElement(tag);
+    contentMain.className = features;
+
+    main1.appendChild(contentMain);
+}
+
+setMain('main', 'main-content');
