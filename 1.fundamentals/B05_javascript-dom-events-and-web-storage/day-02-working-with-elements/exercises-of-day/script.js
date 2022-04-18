8ed69c47e3c5bc79ae0575e07da92e6070beaa6d
Pablo S. Peixoto <pablonegociosnet@gmail.com>
2022-04-18 10:08:12 -0300

    Exercise 2 and 3 ready

--- 1.fundamentals/B05_javascript-dom-events-and-web-storage/day-02-working-with-elements/exercises-of-day/script.js
+++ 1.fundamentals/B05_javascript-dom-events-and-web-storage/day-02-working-with-elements/exercises-of-day/script.js
@@ -7,4 +7,25 @@ function primeiroFilhoBody (texto) {
     paiBody.appendChild(filhoH1);
 }
 
+
+function segundoFilhoBody (className, tag) {
+    let paiBody = document.getElementsByTagName('body')[0];
+
+    let filhoMain = document.createElement(tag);
+    filhoMain.className = className;
+
+    paiBody.appendChild(filhoMain);
+}
+
+function primeiroFilhoMain (className, tag) {
+    let paiMain = document.querySelector('.main-content');
+
+    let filhoSection = document.createElement(tag);
+    filhoSection.className = className;
+
+    paiMain.appendChild(filhoSection);
+}
+
 primeiroFilhoBody('Exercício 5.2 - JavaScript DOM');
+segundoFilhoBody('main-content', 'main');
+primeiroFilhoMain('center-content', 'section');
