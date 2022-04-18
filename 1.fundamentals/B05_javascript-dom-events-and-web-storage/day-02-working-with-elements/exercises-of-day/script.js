b18f6125f5eead0b3357590c169861a449974e1e
Pablo S. Peixoto <pablonegociosnet@gmail.com>
2022-04-18 11:21:50 -0300

    Exercises ready

--- 1.fundamentals/B05_javascript-dom-events-and-web-storage/day-02-working-with-elements/exercises-of-day/script.js
+++ 1.fundamentals/B05_javascript-dom-events-and-web-storage/day-02-working-with-elements/exercises-of-day/script.js
@@ -3,6 +3,7 @@ function primeiroFilhoBody (texto) {
 
     let filhoH1 = document.createElement('h1');
     filhoH1.innerText = texto;
+    filhoH1.className = 'title';
 
     paiBody.appendChild(filhoH1);
 }
@@ -53,9 +54,53 @@ function terceiroFilhoMain (className, tag) {
     paiMain.appendChild(filhoSection);
 }
 
+function imagemSection (className, tag, src) {
+    let paiImagem = document.querySelector('.left-content');
+
+    let filhoImagem = document.createElement(tag);
+    filhoImagem.src = src;
+    filhoImagem.className = className;
+
+    paiImagem.appendChild(filhoImagem);
+}
+
+let numeros = ['um', 'dois','três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez'];
+
+function listaNaoOrdenada () {
+    let paiSection = document.querySelector('.right-content');
+    let listaUl = document.createElement('ul');
+
+    paiSection.appendChild(listaUl);
+
+    let itemLista = document.querySelector('ul');
+
+    for (let index = 0; index < numeros.length; index += 1) {
+        let indice = numeros[index];
+
+        let filhoLi = document.createElement('li');
+        filhoLi.innerText = indice;
+
+        itemLista.appendChild(filhoLi);
+    }
+}
+
+function criaTresTags (qtd, tag) {
+    let paiMain = document.querySelector('.main-content');
+
+    for (let index = qtd; index > 0; index -= 1) {
+        let novaTag = document.createElement(tag);
+        novaTag.className = 'description';
+
+        paiMain.appendChild(novaTag);
+    }
+}
+
 primeiroFilhoBody('Exercício 5.2 - JavaScript DOM');
 segundoFilhoBody('main-content', 'main');
 primeiroFilhoMain('center-content', 'section');
 primeiroFilhoSection('p');
 segundoFilhoMain('left-content', 'section');
 terceiroFilhoMain('right-content', 'section');
+imagemSection ('small-image', 'img', 'https://picsum.photos/200');
+listaNaoOrdenada();
+criaTresTags(3, 'h3');
