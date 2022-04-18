957c1ddaef220a2522929340d6ff43e090e81eb4
Pablo S. Peixoto <pablonegociosnet@gmail.com>
2022-04-18 15:16:03 -0300

    Exercise 1 ready

--- 1.fundamentals/B05_javascript-dom-events-and-web-storage/day-03-javascript-events/course-lessons/main.js
+++ 1.fundamentals/B05_javascript-dom-events-and-web-storage/day-03-javascript-events/course-lessons/main.js
@@ -12,7 +12,12 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
+firstLi.addEventListener('click', classeTech);
 
+function classeTech () {
+    let mudaClasseLi = document.querySelector('li');
+    mudaClasseLi.className = 'tech';
+}
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
