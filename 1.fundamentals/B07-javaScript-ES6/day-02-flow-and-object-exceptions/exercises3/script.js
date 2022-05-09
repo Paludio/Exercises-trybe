dcaa5bd4cb7c5e1eaf3084a848e2eada0844321c
Pablo S. Peixoto <pablonegociosnet@gmail.com>
2022-05-09 20:56:54 -0300

    Exercises 1 the 4 ready

---
+++ 1.fundamentals/B07-javaScript-ES6/day-02-flow-and-object-exceptions/exercises3/script.js
@@ -0,0 +1,45 @@
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+};
+
+const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+};
+
+const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+};
+
+function adiciona (turno) {
+    turno.turno = 'noite';
+}
+
+function lista (objeto) {
+    console.log(Object.keys(objeto));
+}
+
+function tamanho (objeto) {
+    let count = 0;
+    for (const {} in objeto) {
+        count += 1;
+    }
+
+    return count;
+}
+
+function valores (objeto) {
+    console.log(Object.values(objeto));
+}
+
+adiciona(lesson1);
+lista(lesson2);
+console.log(tamanho(lesson3));
+valores(lesson3);
