caca13e2a6fd61b1bdf613dad455efb275b972e5
Pablo S. Peixoto <pablonegociosnet@gmail.com>
2022-05-09 20:00:59 -0300

    Exercise 2 ready

--- 1.fundamentals/B07-javaScript-ES6/day-02-flow-and-object-exceptions/exercises1/script.js
+++ 1.fundamentals/B07-javaScript-ES6/day-02-flow-and-object-exceptions/exercises1/script.js
@@ -14,13 +14,13 @@ function sum() {
     try {
         verifica(value1, value2);
         document.getElementById('result').innerHTML = `Resultado: ${result}`;
-        document.getElementById('value1').value = '';
-        document.getElementById('value2').value = '';
     } catch (error) {
         document.getElementById('result').innerHTML = error.message;
+        
+        console.error(error);
+    } finally {
         document.getElementById('value1').value = '';
         document.getElementById('value2').value = '';
-        console.error(error);
     }
 }
 
