c429f16c9db4460be18e176489e6dddc911fea49
Pablo S. Peixoto <pablonegociosnet@gmail.com>
2022-05-09 19:07:45 -0300

    Exercise 1 ready

--- 1.fundamentals/B07-javaScript-ES6/day-02-flow-and-object-exceptions/exercises/script.js
+++ 1.fundamentals/B07-javaScript-ES6/day-02-flow-and-object-exceptions/exercises/script.js
@@ -1,12 +1,29 @@
+const verifica = (value1, value2) => {
+    if (value1 === '' || value2 === '') {
+        throw new Error('Existe um campo vazio!')
+    }
+}
+
 function sum() {
     const value1 = document.getElementById('value1').value;
     const value2 = document.getElementById('value2').value;
     const result = parseInt(value1) + parseInt(value2);
-    document.getElementById('result').innerHTML = `Resultado: ${result}`;
-    document.getElementById('value1').value = '';
-    document.getElementById('value2').value = '';
-  }
-  window.onload = () => {
+
+    try {
+        verifica(value1, value2);
+        document.getElementById('result').innerHTML = `Resultado: ${result}`;
+        document.getElementById('value1').value = '';
+        document.getElementById('value2').value = '';
+    } catch (error) {
+        document.getElementById('result').innerHTML = error.message;
+        document.getElementById('value1').value = '';
+        document.getElementById('value2').value = '';
+        console.error(error);
+    }
+}
+
+
+window.onload = () => {
     const button = document.getElementById('button');
     button.addEventListener('click', sum);
-  }
+}
