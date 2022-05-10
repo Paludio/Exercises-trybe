e621486342c5e0222c6992e8b0bcfc3b8ff272c3
Pablo S. Peixoto <pablonegociosnet@gmail.com>
2022-05-10 17:58:27 -0300

    Exemplo 2

--- 1.fundamentals/B07-javaScript-ES6/day-03-first-steps-in-jest/jestLesson/aulaAoVivo/script.js
+++ 1.fundamentals/B07-javaScript-ES6/day-03-first-steps-in-jest/jestLesson/aulaAoVivo/script.js
@@ -2,4 +2,16 @@ function sum(a, b) {
     return a + b;
 }
 
-module.exports = sum;
+const somaNumero = (numero) => {
+    let contador = 0;
+
+    for(let index = 0; index <= numero; index += 1) {
+        contador += index;
+    }
+    return contador;
+};
+
+module.exports = {
+    sum,
+    somaNumero
+};
--- 1.fundamentals/B07-javaScript-ES6/day-03-first-steps-in-jest/jestLesson/aulaAoVivo/script.test.js
+++ 1.fundamentals/B07-javaScript-ES6/day-03-first-steps-in-jest/jestLesson/aulaAoVivo/script.test.js
@@ -1,13 +1,30 @@
-const sum = require('./script');
+const { sum, somaNumero } = require('./script');
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-}); 
-
-test('Somando 0 + 0 igual a 0', () => {
-    expect(sum(0, 0)).toBe(0);
+describe('Teste da função sum', () => {
+    test('adds 1 + 2 to equal 3', () => {
+        expect(sum(1, 2)).toBe(3);
+    }); 
+    
+    test('Somando 0 + 0 igual a 0', () => {
+        expect(sum(0, 0)).toBe(0);
+    });
+    
+    test('Somando 0.5 + 0.8 igual a 1.3', () => {
+        expect(sum(0.5, 0.8)).toBeCloseTo(1.3);
+    });
+});
+ 
+describe('Teste da função somaNumero', () => {
+    it('Espera que a soma de 1 seja igual a 1', () => {
+        expect(somaNumero(1)).toBe(1);
+    });
+    
+    it('Espera que a soma de 3 seja igual a 6', () => {
+        expect(somaNumero(3)).toBe(6);
+    });
+    
+    it('Espera que a soma de 5 seja igual a 15', () => {
+        expect(somaNumero(5)).toBe(15);
+    });
 });
 
-test('Somando 0.5 + 0.8 igual a 1.3', () => {
-    expect(sum(0.5, 0.8)).toBeCloseTo(1.3);
-}); 
